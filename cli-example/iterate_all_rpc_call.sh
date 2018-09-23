b89bc05586c2c7e053610de52bdd575c04e9a1e7
NIC619 <twedusuck@gmail.com>
2018-09-23 14:30:14 +0800

    Call directly

--- cli-example/iterate_all_rpc_call.sh
+++ cli-example/iterate_all_rpc_call.sh
@@ -78,27 +78,29 @@ done
 sleep 2
 
 # peer 0 add peer 1
-add_peer 0 1
+# add_peer 0 1
+./sharding-p2p-poc -rpcport=$((RPCPORT+0)) -client addpeer $IP $((PORT+1)) 1
 
 # peer 0 subscribe shard
-subscribe_shard 0 1 2 3 4 5
+# subscribe_shard 0 1 2 3 4 5
+./sharding-p2p-poc -rpcport=$((RPCPORT+0)) -client subshard 1 2 3 4 5
 
-# peer 1 subscribe shard
-subscribe_shard 1 2 3 4
+# # peer 1 subscribe shard
+# subscribe_shard 1 2 3 4
 
-# get peer 0's subscribed shard
-get_subscribe_shard 0
+# # get peer 0's subscribed shard
+# get_subscribe_shard 0
 
-# peer 0 broadcast collations
-broadcast_collation 0 2 2 100 0
+# # peer 0 broadcast collations
+# broadcast_collation 0 2 2 100 0
 
-# peer 1 broadcast collations
-broadcast_collation 1 1 1 100 0
+# # peer 1 broadcast collations
+# broadcast_collation 1 1 1 100 0
 
-# peer 0 unsubscribe shard
-unsubscribe_shard 0 2 4
+# # peer 0 unsubscribe shard
+# unsubscribe_shard 0 2 4
 
-get_subscribe_shard 0
+# get_subscribe_shard 0
 
 for i in `seq 0 1`;
 do
